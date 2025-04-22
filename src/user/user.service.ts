971b861c12426ecc77017804c5770182d017aa03
¨Gilson <gilsonmeira97@gmail.com>
2025-04-22 20:46:55 +0200

    created fuction to verify if the user already exists, also created the function to create this user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,28 +2,48 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma.service';
+import { ResponseEntity } from './entities/ResponseEntity.entity';
+import { User } from './entities/User.entity';
+import * as bcrypt from 'bcrypt';
+
+
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
   
-  create(createUserDto: CreateUserRequest) {
-    return createUserDto;
-  }
-
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async create(userRequest: CreateUserRequest) {
+    let response: ResponseEntity = await this.verifyIfUserExists(userRequest);
+    if(response.hasMessage()) return response;
+    
+    return response = await this.registerUser(userRequest);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+
+  private async verifyIfUserExists(userRequest: CreateUserRequest) {
+    const user = await this.prisma.user.findFirst({where: {email: userRequest.email} });
+    console.log(user)
+    if(user != null) return ResponseEntity.error(["Email is already in use."])
+    else return new ResponseEntity()
+  }
+
+  private async registerUser(userRequest: CreateUserRequest) {
+    const response: ResponseEntity = new ResponseEntity();
+
+    // Maps the CreateUserRequest to a User
+    const newUser: User = userRequest.mapperToUser();
+    // Encrypt the password in the new User
+    newUser.password = await bcrypt.hash(userRequest.password, 10)
+    try{
+      const savedUser = await this.prisma.user.create({data: newUser})
+      return response.setData({id: savedUser.id, email: savedUser.email, name: savedUser.name})
+    } catch(error: any) {
+      return response.setMsg(["Error to create new user.", error.message])
+    }
+    
   }
 }
