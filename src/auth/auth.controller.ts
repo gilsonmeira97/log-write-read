5643a54e73a8792fc518263c36850f405f4add17
¨Gilson <gilsonmeira97@gmail.com>
2025-04-22 21:51:48 +0200

    created controller to /auth/login

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { UserValidator } from 'src/user/dto/user-validator.dto';
+import { LoginUserRequest } from './dto/login-user.dto';
+import { Response } from 'express';
+import { ResponseEntity } from 'src/user/entities/ResponseEntity.entity';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService ){}
+
+    @Post("/login")
+    async login(@Body() request: LoginUserRequest, @Res() res: Response) {
+        let response: ResponseEntity = new ResponseEntity();
+        const userValidator = new UserValidator().loginUser(request);
+        if(userValidator.hasError()) {
+            response.setMsg(userValidator.messages)
+            return res.status(400).json(response)
+        }
+        
+        response = await this.authService.login(request)
+
+        return res.status( response.hasMessage() ? 400 : 200 ).json(response)
+
+    }
+}
