ba48a7246ee9db08d0d5bc8ed0b1633e0d0e254f
¨Gilson <gilsonmeira97@gmail.com>
2025-04-23 21:34:05 +0200

    AuthGuard implementation, routes marked with the @public decorator are excluded from guard protection, token validation and handling of possible errors.

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,43 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { IS_PUBLIC_KEY } from 'src/decorators/public.decorator';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private jwtService: JwtService, private configService: ConfigService, private reflector: Reflector) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ])
+    
+    // Checks if the current request is directed to a controller method decoreded with @Public
+    // If true, authorize the access without token
+    if(isPublic) return true;
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+    
+    if (!token) throw new UnauthorizedException(null, "Invalid token.");
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
+      request['user'] = payload;
+    } catch (error) {
+      throw new UnauthorizedException("Token validation failed.");
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request) {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : null;
+  }
+}
