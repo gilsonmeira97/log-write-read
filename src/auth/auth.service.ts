a5b8401647203e37a0feb0aa761ddc96d978512e
¨Gilson <gilsonmeira97@gmail.com>
2025-04-23 18:51:17 +0200

    Implemented generation token to return on response.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { ResponseEntity } from 'src/user/entities/ResponseEntity.entity';
 import * as bcrypt from 'bcrypt';
-
-type Login = {email: string, password: string}
+import { JwtService } from '@nestjs/jwt';
+import { LoginUserRequest } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService){}
+    constructor(private prisma: PrismaService, private jwtService: JwtService) { }
 
-    async login({email, password}: Login) {
+    async login(loginUser: LoginUserRequest) {
         const response: ResponseEntity = new ResponseEntity();
-        const findedUser = await this.prisma.user.findUnique({where: {email: email}});
+        const findedUser = await this.prisma.user.findUnique({ where: { email: loginUser.email } });
 
-        if(findedUser && await bcrypt.compare(password, findedUser.password)) {
-            return response.setData(true);
+        if (findedUser && await bcrypt.compare(loginUser.password, findedUser.password)) {
+            const payload = { sub: findedUser.id, email: findedUser.email }
+            return response.setData(await this.jwtService.signAsync(payload));
         } else {
             return response.addMsg("Email or password invalid.");
         }
