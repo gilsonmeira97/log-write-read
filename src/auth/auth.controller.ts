c66b41b80f16af7bf40bb2a72cc3f7d77ef13880
¨Gilson <gilsonmeira97@gmail.com>
2025-04-23 21:31:04 +0200

    Just ; ajusts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,23 +4,25 @@ import { LoginUserRequest } from './dto/login-user.dto';
 import { Response } from 'express';
 import { ResponseEntity } from 'src/user/entities/ResponseEntity.entity';
 import { AuthService } from './auth.service';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService ){}
 
+    @Public()
     @Post("/login")
     async login(@Body() request: LoginUserRequest, @Res() res: Response) {
         let response: ResponseEntity = new ResponseEntity();
         const userValidator = new UserValidator().loginUser(request);
         if(userValidator.hasError()) {
-            response.setMsg(userValidator.messages)
-            return res.status(400).json(response)
+            response.setMsg(userValidator.messages);
+            return res.status(400).json(response);
         }
         
-        response = await this.authService.login(request)
+        response = await this.authService.login(request);
 
-        return res.status( response.hasMessage() ? 400 : 200 ).json(response)
+        return res.status( response.hasMessage() ? 400 : 200 ).json(response);
 
     }
 }
