5b43b3a032eeba22122dd1b489f8125869ed0b4c
¨Gilson <gilsonmeira97@gmail.com>
2025-04-23 18:52:40 +0200

    Added import from JwtModule to be abble to use on AuthService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '2h' }
+      }),
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService]
 })
