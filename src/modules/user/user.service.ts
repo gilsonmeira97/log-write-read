033b0595ea617cb444a886f26e894d6158f6555f
Gilson Meira de Mendonca <gilsonmeira97@gmail.com>
2025-04-24 16:07:20 +0200

    Created method service that is used on update user controller.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,28 +5,47 @@ import { PrismaService } from 'src/prisma.service';
 import { ResponseEntity } from '../../common/entities/ResponseEntity.entity';
 import { User } from './entities/User.entity';
 import * as bcrypt from 'bcrypt';
+import { UserToken } from '../auth/entities/UserToken';
 
 
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
-  
+  constructor(private prisma: PrismaService) { }
+
   async create(userRequest: CreateUserRequest) {
     let response: ResponseEntity = await this.verifyIfUserExists(userRequest);
-    if(response.hasMessage()) return response;
-    
+    if (response.hasMessage()) return response;
+
     return response = await this.registerUser(userRequest);
   }
 
-  update(updateUserDto: UpdateUserRequest) {
-    return `This action updates a user`;
+  async update(userRequest: UpdateUserRequest, userToken: UserToken) {
+    let response: ResponseEntity = new ResponseEntity();
+    try {
+      const formatedUser = await this.formatUpdateUser(userRequest);
+      const updatedUser = await this.prisma.user.update({ 
+        where: { id: userToken.id }, 
+        data: {...formatedUser} 
+      })
+      return response.setData({name: updatedUser.name, email: updatedUser.email});
+    } catch (error) {
+      return response.addMsg("Failed to update user.")
+    }
   }
 
+  async isInstalled() {
+    const totalUsers = await this.prisma.user.count();
+    return totalUsers != 0;
+  }
+
+  private async encryptPassword(password: string) {
+    return await bcrypt.hash(password, 10);
+  }
 
   private async verifyIfUserExists(userRequest: CreateUserRequest) {
-    const user = await this.prisma.user.findFirst({where: {email: userRequest.email} });
-    if(user != null) return ResponseEntity.error(["Email is already in use."])
+    const user = await this.prisma.user.findFirst({ where: { email: userRequest.email } });
+    if (user != null) return ResponseEntity.error(["Email is already in use."])
     else return new ResponseEntity()
   }
 
@@ -36,17 +55,20 @@ export class UserService {
     // Maps the CreateUserRequest to a User
     const newUser: User = userRequest.mapperToUser();
     // Encrypt the password in the new User
-    newUser.password = await bcrypt.hash(userRequest.password, 10);
-    try{
-      const savedUser = await this.prisma.user.create({data: newUser});
-      return response.setData({id: savedUser.id, email: savedUser.email, name: savedUser.name});
-    } catch(error: any) {
+    newUser.password = await this.encryptPassword(userRequest.password);
+    try {
+      const savedUser = await this.prisma.user.create({ data: newUser });
+      return response.setData({ id: savedUser.id, email: savedUser.email, name: savedUser.name });
+    } catch (error: any) {
       return response.setMsg(["Error to create new user.", error.message]);
     }
   }
-  
-  async isInstalled() {
-    const totalUsers = await this.prisma.user.count();
-    return totalUsers != 0;
+
+  private async formatUpdateUser(userRequest: UpdateUserRequest) {
+    let formatedUser: UpdateUserRequest = {}
+    if(userRequest.name) formatedUser.name = userRequest.name;
+    if(userRequest.password) formatedUser.password = await this.encryptPassword(userRequest.password);
+    return formatedUser;
   }
+
 }
